8bcddc4d8f3249234680d3407fac120f1cd52c45
Sam <gmc103709@gmail.com>
2022-11-14 11:55:23 -0300

    changed color

--- src/components/ComponentChat/style.ts
+++ src/components/ComponentChat/style.ts
@@ -1,54 +1,47 @@
 import styled from "styled-components";
 
-
-
-
 export const ContainerMessages = styled.div`
-display: flex;
-padding: 1rem;
+  display: flex;
+  padding: 1rem;
 
-
-&::-webkit-scrollbar-track {
-    background-color: #F4F4F4;
-}
-&::-webkit-scrollbar {
+  &::-webkit-scrollbar-track {
+    background-color: #f4f4f4;
+  }
+  &::-webkit-scrollbar {
     width: 10px;
     background: #f5f5f5;
-}
-&::-webkit-scrollbar-thumb {
+  }
+  &::-webkit-scrollbar-thumb {
     background: #dad7d7;
-}
+  }
 
-
-
-    img {
+  img {
     width: 50px;
     height: 50px;
     border-radius: 10px;
+  }
+
+  div {
+    flex-direction: column;
+    flex: 1;
+    padding: 0 1rem 0 1rem;
+
+    p {
+      word-break: break-all;
+      margin-top: 10px;
     }
+  }
 
-        div {
-            flex-direction: column;
-            flex: 1;
-            padding: 0 1rem 0 1rem;
+  button {
+    background: transparent;
+    border: 0;
+    svg {
+      color: #f0f0f5;
+      transition: 0.2s;
 
-            p {
-                word-break: break-all;
-                margin-top: 10px;
-            }
-        }
-        
-        button {
-            background: transparent;
-            border: 0;
-            svg {
-                color: #f0f0f5;
-                transition: 0.2s;
-
-                &:hover {
-                    color: #a8a8b3;
-                }
-            }
-        }
-    
-`
+      &:hover {
+        color: #a8a8b3;
+      }
+    }
+  }
+`;
